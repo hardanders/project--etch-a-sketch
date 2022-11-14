a2d70ebff2f0bce487bc308f00b603cedbb6f606
hardanders <andyyy85@gmail.com>
2022-11-14 15:58:51 -0500

    Completed base functions, added button to change number of squares, modified in place logic

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 let size = 16;
+let chosenColor = 'red';
 const container = document.querySelector('.container');
-chosenColor = 'red';
-
+const sizeBtn = document.querySelector('.pixel-button');
 
 window.addEventListener('load', () => {
     createGrid();
-    addListeners();
 });
+
+sizeBtn.addEventListener('click', () => {
+    let selection = prompt("How many squares on each side?");
+    size = selection;
+    container.innerHTML = '';
+    createGrid();
+})
+
 
 function createGrid() {
     for (i = 1; i <= size; i++) {
@@ -16,6 +23,8 @@ function createGrid() {
     conArray.forEach(con => {
         createRow(con);
     })
+    addListeners();
+    setGridSize();
 }
 
 function createRow(con) {
@@ -25,14 +34,29 @@ function createRow(con) {
 }
 
 function addListeners() {
-    const colArray = Array.from(document.querySelectorAll('.column'));
+    let colArray = Array.from(document.querySelectorAll('.column'));
     for (let col of colArray) {
-        col.addEventListener('click', () => {
-            color(col);
-        });
+            col.addEventListener('mouseover', () => {
+                toggleColor(col);
+            });
+        }
     }
+
+function toggleColor(col) {
+    /* if (col.style.backgroundColor != chosenColor) { */
+        col.style.backgroundColor = chosenColor;
+/*     } else {
+        col.style.backgroundColor = document.body.style.backgroundColor;
+    } */
 }
 
-function color(col) {
-    col.style.backgroundColor = chosenColor;
+function setGridSize() {
+    const getWidth = getComputedStyle(container).width;
+    const width = getWidth.split('px');
+    const sq = getComputedStyle(document.querySelector('.column')).border.slice(0, 1);
+    const colArray = Array.from(document.querySelectorAll('.column'));
+    for (let col of colArray) {
+        col.style.width = (width[0] / size - (sq*2)) + 'px';
+        col.style.height = (width[0] / size - (sq*2)) + 'px';
+    }
 }
