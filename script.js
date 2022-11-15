1143c8130e3781517ea5f37e98acffeafa8306b8
hardanders <andyyy85@gmail.com>
2022-11-15 10:47:37 -0500

    Refactored sizeBtn and completed initial testing - all functional to TOP standards

--- script.js
+++ script.js
@@ -2,20 +2,83 @@ let size = 16;
 let chosenColor = 'red';
 const container = document.querySelector('.container');
 const sizeBtn = document.querySelector('.pixel-button');
+const blackBtn = document.querySelector('.black');
+const redBtn = document.querySelector('.red');
+const rgbBtn = document.querySelector('.rgb');
+const eraser = document.querySelector('.eraser');
+const reset = document.querySelector('.reset');
 
 window.addEventListener('load', () => {
     createGrid();
 });
 
 sizeBtn.addEventListener('click', () => {
-    let selection = prompt("How many squares on each side?");
-    size = selection;
-    container.innerHTML = '';
+    changeSize(requestNum());
+})
+
+blackBtn.addEventListener('click', () => {
+    addListeners();
+    chosenColor = 'black';
+})
+
+redBtn.addEventListener('click', () => {
+    addListeners();
+    chosenColor = 'red';
+})
+
+rgbBtn.addEventListener('click', () => {
+    rainbow();
+})
+
+eraser.addEventListener('click', () => {
+    addListeners;
+    chosenColor = 'white';
+})
+
+reset.addEventListener('click', () => {
     createGrid();
 })
 
+function requestNum() {
+    return selection = Number(prompt("How many squares on each side? (Up to 100)"));
+}
+
+function changeSize(num) {
+    if (isNaN(num) || num <= 0 || num == '') {
+        alert('Pick a number!!');
+    } else if (num > 100) {
+        alert('Make it less than 100!!');
+    } else {
+        size = selection;
+        container.innerHTML = '';
+        createGrid();
+    }
+}
+
+function randomColor() {
+    let randomNum = Math.floor(Math.random() * 16777215).toString(16);
+    let randomColor = '#' + randomNum;
+    return randomColor;
+}
+
+function rainbow() {
+    const newCon = document.getElementsByClassName('container');
+    for (let i = 0; i < newCon.length; i++) {
+        newCon[i].addEventListener("mouseover", handlemouseover);
+        newCon[i].addEventListener("mouseout", handlemouseout);
+    }
+}
+
+function handlemouseover() {
+    chosenColor = randomColor();
+}
+
+function handlemouseout() {
+    chosenCOlor = randomColor();
+}
 
 function createGrid() {
+    container.innerHTML = '';
     for (i = 1; i <= size; i++) {
         container.appendChild(document.createElement('div')).classList.add(`row`);
     }
